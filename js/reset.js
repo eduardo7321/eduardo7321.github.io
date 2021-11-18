caa0b91f713fcbfe50f6ca9115890947495a2704
Eduardo Silva <Eduardo.silva@fml.ind.br>
2021-11-18 17:38:16 -0300

    adicionando funcao touchstart no botao reset

--- js/reset.js
+++ js/reset.js
@@ -1,6 +1,14 @@
 var btn = document.querySelector("#reset");
 
-btn.addEventListener("click ontouchstart", function() {
+btn.addEventListener("touchstart", function() {
+    
+    location.reload();
+
+});
+
+var btn = document.querySelector("#reset");
+
+btn.addEventListener("click", function() {
     
     location.reload();
 
